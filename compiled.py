46103bbc41eec6c7975d8808861147a765ff36a5
Daniel Akama <57869416+Daniel1999Akama@users.noreply.github.com>
2024-11-09 11:20:36 -0500

    Update compiled.py

--- compiled.py
+++ compiled.py
@@ -173,5 +173,9 @@ def chatbot():
 
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
